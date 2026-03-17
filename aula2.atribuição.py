e355c8e8c5e959b25102b1eea61431e5f7f22d8c
Anny Luz <annycarolynesouza248@gmail.com>
2026-03-17 15:07:43 -0300

    Create aula2.atribuição.py

---
+++ aula2.atribuição.py
@@ -0,0 +1,15 @@
+n1=int(input("Digite um número: "))
+n2=int(input("Digite outro número: "))
+
+soma = n1 + n2
+subtracao = n1 - n2
+multiplicacao = n1 * n2
+divisao = n1 / n2
+
+print("A soma entre eles é igual a: ", soma)
+
+print("A subtração entre eles é igual a: ", subtracao)
+
+print("A multiplicação entre eles é igual a: ", multiplicacao)
+
+print("A divisão entre eles é igual a: ", divisao)
